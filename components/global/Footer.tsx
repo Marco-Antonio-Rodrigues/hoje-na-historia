1505440845ba3ff745eb61555684096b2049cf19
leviobrabo <carlosjunior20313@gmail.com>
2024-08-30 17:43:59 -0300

    alteração do link do telegram

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -4,7 +4,7 @@ import { IconTelegram } from "../Icon"
 const Footer = () => {
   return (
     <footer className="w-full dark:text-CustomAntiqueWhite text-CustomCharcoal lg:text-lg flex items-center justify-center py-10">
-        <Link href='https://web.telegram.org/k/#@historia_br' className="lg:no-underline hover:underline flex gap-2 items-center transition-transform duration-700 hover:scale-105">
+        <Link href='https://t.me/historia_br' className="lg:no-underline hover:underline flex gap-2 items-center transition-transform duration-700 hover:scale-105">
           <IconTelegram/>
           historia_br
         </Link>
