2c43d1e7c181bed3558b8a43856d342b03023407
Marco Antonio Rodrigues Gomes de Souza <94053824+Marco-Antonio-Rodrigues@users.noreply.github.com>
2024-08-30 13:00:29 -0300

    ✨: alterando link do canal

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -4,9 +4,9 @@ import { IconTelegram } from "../Icon"
 const Footer = () => {
   return (
     <footer className="w-full dark:text-CustomAntiqueWhite text-CustomCharcoal lg:text-lg flex items-center justify-center py-10">
-        <Link href='https://t.me/historiadehojebr/4948' className="lg:no-underline hover:underline flex gap-2 items-center transition-transform duration-700 hover:scale-105">
+        <Link href='https://web.telegram.org/k/#@historia_br' className="lg:no-underline hover:underline flex gap-2 items-center transition-transform duration-700 hover:scale-105">
           <IconTelegram/>
-          historiadehojebr
+          historia_br
         </Link>
     </footer>
   )
